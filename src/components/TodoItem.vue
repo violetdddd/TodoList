e93886ed61b0fef58d72ce85acdfa8f9d6350866
Junduo Zheng <1253314323@qq.com>
2023-07-21 22:31:33 +0800

    done turn gray

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="pt-1 flex flex-row gap-2">
-        <h3 class="w-full p-2 rounded-lg border border-gray-400 text-xl text-gray-900 bg-yellow-50">{{ msg }}</h3>
-        <button @click="$emit('delete')" class="flex-1 px-4 py-2 rounded-lg font-semibold text-md text-white bg-green-600 hover:bg-green-700 focus:ring-2 focus:ring-green-300">Done</button>
+        <h3 class="w-full p-2 rounded-lg border border-gray-400 text-xl text-gray-900"
+        :class="{'bg-gray-500 text-gray-300':turnGray,' bg-yellow-50':!turnGray}">{{ msg }}</h3>
+        <button @click="turnGray = !turnGray" class="flex-1 px-2 py-2 rounded-lg font-semibold text-md  focus:ring-2"
+        :class="{'bg-gray-500 focus:ring-gray-400 text-gray-300' : turnGray,' bg-green-600  focus:ring-green-300 text-white hover:bg-green-700':!turnGray}">Done</button>
+        <button @click="$emit('delete')" class="flex-1 px-2 py-2 rounded-lg font-semibold text-md text-white bg-rose-600 hover:bg-rose-700 focus:ring-2 focus:ring-rose-300">Delete</button>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
+const turnGray=ref(false)
 
 defineProps({
     msg:{
